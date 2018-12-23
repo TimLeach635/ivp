3e969422345f6e86827396a25068b12d4d78a612
Tim Leach <timleach635@gmail.com>
2018-12-23 19:23:15 +0000

    Began 'generate_runge_kutta' method, and updated README.

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # ivp
 A collection of numerical methods for solving Initial Value Problems (IVPs). Work in progress.
 
-# Current features
-* Explicit and implicit Euler methods for one-dimensional ODEs.
+## Current features
+* Explicit and implicit Euler methods for scalar ODEs
 
-# Planned features
-* Multidimensional ODEs for the above.
-* Generic Runge-Kutta methods.
-* Finite Element Methods for multidimensional IVPs.
+## Planned features
+* Extend Euler's methods to vector ODEs
+* Generic Runge-Kutta methods
+* Multi-step methods
+* Finite Element Methods for multidimensional PDE IVPs
+
+## Possible future features
+* Choices for iteration method used in implicit schemes
--- one_step.py
+++ one_step.py
@@ -140,6 +140,138 @@ def implicit_euler(x_0, y_0, h, n_iter, kind='nonlinear', **kwargs):
     return xs, ys
 
 
+def generate_runge_kutta(a, b):
+    """Generate the Runge-Kutta method from the given Butcher table.
+
+    The Butcher table takes this form:
+
+     c | a
+    ___|___
+       |  T
+       | b
+
+    with 'a' an n x n square matrix, and 'b' an n x 1 column vector, and 'c', another column vector,
+    the row sums of 'a'.
+
+    Parameters:
+        a - a scalar, or n x n array.
+        b - a scalar, or n x 1 array.
+
+    Returns:
+        A function with the following signature:
+        xs, ys = rk_method(x_0, y_0, h, **kwargs)
+
+        Positional arguments:
+            x_0 and y_0 are the given initial values: y(x_0) = y_0.
+            h is the grid spacing.
+
+        Keyword arguments:
+            either define 'n_pts' or 'x_max'.
+            n_pts is the number of grid points (not including the origin), and
+            x_max is the x-value of the largest grid point.
+
+            either define 'f', or 'p' and 'q'.
+            if 'f' is defined, it should be a 2-argument function, and the method solves the nonlinear ODE
+            dy/dx = f(x, y).
+            if the R-K method you define is implicit, you must also define 'f_prime', the derivative with respect to y
+            of 'f'.
+            if 'p' and 'q' are defined, they should both be 1-argument functions, and the method will solve the linear
+            ODE dy/dx + p(x)*y = q(x).
+
+            define 'n_iter' if the R-K method defined is implicit.
+            it represents the number of iteration steps in the method used to solve the implicit bit.
+
+        Returns:
+            xs and ys, two numpy arrays holding the x and y coordinates of the numerical solution.
+            matplotlib-able.
+    """
+    # is it just scalars?
+    try:
+        a = float(a)
+        b = float(b)
+        # it's got this far so they're all scalar
+        n_stages = 1
+        c = a
+    except TypeError:
+        # they are arrays, check size compatibility
+        # check dimensionality
+        if not (a.ndim == b.ndim == 2):
+            raise TypeError("'a' and 'b' should both be 2-dimensional.")
+
+        # then, ensure they are both column vectors
+        if not b.shape[2] == 1:
+            raise TypeError("'b' should be a column vectors.")
+
+        # number of stages is then this
+        n_stages = b.shape[1]
+
+        # make sure a fits also
+        if not (a.shape[1] == a.shape[2] == n_stages):
+            raise TypeError("'a' must be square and compatible with 'b'.")
+
+        # set c
+        c = a.sum(1)
+
+    # explicit or implicit?
+    explicit = False
+    if n_stages == 1:
+        if a == 0:
+            explicit = True
+    else:
+        # check strictly lower triangular
+        if a == np.tril(a, -1):
+            explicit = True
+
+    # form function
+    if explicit:
+        if n_stages == 1:
+            def rk_method(x_0, y_0, h, **kwargs):
+                """Solve the given one-dimensional ODE.
+
+                :param x_0:
+                :param y_0:
+                :param h:
+                :param kwargs:
+                :return (xs, ys): two numpy arrays of the same length representing the numerical solution.
+                """
+
+                # linear or nonlinear
+                if 'p' in kwargs.keys() and 'q' in kwargs.keys():
+                    linear = True
+                    p = kwargs['p']
+                    q = kwargs['q']
+                elif 'f' in kwargs.keys():
+                    linear = False
+                    f = kwargs['f']
+                else:
+                    raise TypeError("You must pass keyword arguments 'p' and 'q', or 'f'.")
+
+                # number of points
+                if 'n_pts' in kwargs.keys():
+                    n_pts = kwargs['n_pts']
+                elif 'x_max' in kwargs.keys():
+                    n_pts = int(kwargs['x_max']/h)
+                else:
+                    raise TypeError("You must pass either keyword arguments 'n_pts' or 'x_max'.")
+
+                # form output
+                xs = np.linspace(x_0, x_0 + h*n_pts, n_pts)
+                ys = [y_0]
+
+                # solve
+                if linear:
+                    for i in range(n_pts-1):
+                        ys.append(ys[i] + h*b*(q(xs[i]) - p(xs[i])*ys[i]))
+                else:
+                    for i in range(n_pts-1):
+                        ys.append(ys[i] + h*b*f(xs[i], ys[i]))
+
+                return xs, np.array(ys)
+
+    # return function
+    return rk_method
+
+
 def root_find(f, f_prime, init_guess, steps):
     x = init_guess
     for _ in range(steps):
